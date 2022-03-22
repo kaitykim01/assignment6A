e765022b71dd3c4f514b821fe2dce4668b9d8e65
kaitykim01 <kaitykim01@gmail.com>
2022-03-22 01:46:39 -0400

    fixed addToCart console

--- main.js
+++ main.js
@@ -8,34 +8,39 @@ function showPopup(txt, times) {
         createButton(times[i]); }
 }
 
-let cartList; // initialize cartlist //
+function closePopup() {
+    document.getElementById('popup-screen').style.display='none';
+    document.getElementById('next').style.display='block';
+}
+
+let cartList = []; // initialize cartlist //
 const storedValue = JSON.parse(localStorage.getItem('savedApptDate'));
 const storedTime = JSON.parse(localStorage.getItem('savedApptTime'));
 if (storedValue == null + storedTime == null) {
     cartList = [];
+
 } else {
-    cartList = [storedValue, storedTime];
+    //cartList.push(storedValue);
+    //cartList.push(storedTime);
 } 
 
-
+function addToCart() {
+    cartList.push(JSON.parse(localStorage.getItem('savedApptDate'))); 
+    cartList.push(JSON.parse(localStorage.getItem('savedApptTime')));
+    console.log("Cart list: ", cartList); 
+}
 function createButton(apptTime) {
     let btn = document.createElement('button');
     btn.innerHTML = apptTime;
     btn.className = 'button button-hrs';
     btn.onclick = function () {
-        localStorage.setItem('savedApptTime', JSON.stringify(apptTime));
-        console.log(cartList);
+        localStorage.setItem('savedApptTime', JSON.stringify(apptTime));  
     }
-    console.log(cartList);
     document.getElementById('timeBox').appendChild(btn);
 }
 
-
-
 function confirmAppt() {
-    document.getElementById('after-colon1').innerHTML = 'Date/Time: ' + cartList[cartList.length-2] + " @ " + cartList[cartList.length-1];
-    console.log(cartList);
-
+    document.getElementById('after-colon1').innerHTML = 'Date/Time: ' + storedValue + " @ " + storedTime;
 }
 
 

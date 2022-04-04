6e3e6bc59848ecafd712bc3526c3eec4b4aef6a5
kaitykim01 <kaitykim01@gmail.com>
2022-04-04 11:29:52 -0400

    6B remove cart function & cartList updates across pages

--- main.js
+++ main.js
@@ -3,6 +3,7 @@ function showPopup(txt, times) {
     document.getElementById('popup-screen').style.display='block';
     document.getElementById('dateBox').innerHTML = txt;
     document.getElementById('timeBox').innerHTML ='';
+    localStorage.setItem('cartList', JSON.stringify([storedValue, storedTime])); // initialize cartlist //
     localStorage.setItem('savedApptDate', JSON.stringify(txt));
     for (let i=0; i<times.length; i++) {
         createButton(times[i]); }
@@ -13,21 +14,16 @@ function closePopup() {
     document.getElementById('next').style.display='block';
 }
 
-let cartList = []; // initialize cartlist //
-const storedValue = JSON.parse(localStorage.getItem('savedApptDate'));
-const storedTime = JSON.parse(localStorage.getItem('savedApptTime'));
-if (storedValue == null + storedTime == null) {
-    cartList = [];
-
-} else {
-    //cartList.push(storedValue);
-    //cartList.push(storedTime);
-} 
+var cartList = JSON.parse(localStorage.getItem('cartList'));
+var storedValue = JSON.parse(localStorage.getItem('savedApptDate'));
+var storedTime = JSON.parse(localStorage.getItem('savedApptTime'));
 
 function addToCart() {
-    cartList.push(JSON.parse(localStorage.getItem('savedApptDate'))); 
-    cartList.push(JSON.parse(localStorage.getItem('savedApptTime')));
+    cartList.push(storedValue);
+    cartList.push(storedTime);
     console.log("Cart list: ", cartList); 
+    displayList(storedValue, storedTime);
+
 }
 function createButton(apptTime) {
     let btn = document.createElement('button');
@@ -41,9 +37,32 @@ function createButton(apptTime) {
 
 function confirmAppt() {
     document.getElementById('after-colon1').innerHTML = 'Date/Time: ' + storedValue + " @ " + storedTime;
+    console.log("Cart List works across pages!", cartList);
 }
 
+/// Appointments List from Cart ///
+var count = 0;
+function displayList(day, time) {
+    let list = document.getElementById('apptList');
+    let li = document.createElement('li');
+    li.id = 'buttonForList'+count;
+    li.innerText = (day + "@" + time);
+    list.appendChild(li);
+    count = count + 1; 
+    console.log(count);
 
+    let list2 = document.getElementById(li.id);
+    let btn = document.createElement('button');
+    btn.innerText = "Cancel Appt";
+    btn.onclick = removeAppt;
+    list2.appendChild(btn);
+}
 
-
-
+//Remove from cartList function
+function removeAppt() {
+    cartList.splice(count-1, 2);
+    console.log('removed', cartList, count)
+    let list=document.getElementById('buttonForList'+(count-1));
+    list.innerHTML='';
+    
+}
